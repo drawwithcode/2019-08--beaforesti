b6f849adfd08d0d9d1c4c4714723cfb4db61b5fe
beaforesti <bea_fiore@live.it>
2019-11-09 16:06:41 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,44 @@
-function preload(){
-  // put preload code here
+var myMap;
+var canvas;
+var position;
+var mappa = new Mappa('MapboxGL', "pk.eyJ1IjoiYmVhZmlvcmUiLCJhIjoiY2sycm93NWV1MHBuZjNibDRvNHg0dWt0biJ9.z9oz2PuAy578jdhkKEdKlQ");
+
+var poliLat = 45.5048188;
+var poliLon = 9.1629839;
+
+var options = {
+  lat: poliLat,
+  lng: poliLon,
+  zoom: 8, //map is centered in the point 0, 0, zoom level 4
+  style: "mapbox://styles/mapbox/traffic-night-v2",
+  // style: "mapbox://styles/beafiore/ck2rncqlt3lrv1cmkf6lw37g9",
+}
+
+function preload() {
+  position = getCurrentPosition();
 }
 
 function setup() {
-  // put setup code here
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+
 }
 
 function draw() {
-  // put drawing code here
+  clear()
+
+  var here = myMap.latLngToPixel(position.latitude, position.longitude);
+  fill(color("#ff6600"));
+  // strokeWeight(3);
+  textSize(14);
+  ellipse(here.x, here.y, 30)
+  text('Here you are', here.x - 40, here.y - 20);
+
+  var poli = myMap.latLngToPixel(poliLat, poliLon);
+  fill(color("#0066ff"));
+  ellipse(poli.x, poli.y, 30);
+  text('Politecnico di Milano', poli.x - 60, poli.y - 20);
+
 }
