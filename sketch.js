ddd4e514eeb26b0551d34d19a06e2147a60704e9
beaforesti <bea_fiore@live.it>
2019-11-12 23:29:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
+var col1 = "#ff6600";
 var myMap;
 var canvas;
 var position;
 var mappa = new Mappa('MapboxGL', "pk.eyJ1IjoiYmVhZmlvcmUiLCJhIjoiY2sycm93NWV1MHBuZjNibDRvNHg0dWt0biJ9.z9oz2PuAy578jdhkKEdKlQ");
 
-var poliLat = 45.5048188;
-var poliLon = 9.1629839;
+// Monte Bianco coordinates
+var monteBiancoLat = 45.5048188;
+var monteBiancoLon = 6.8564201;
 
 var options = {
-  lat: poliLat,
-  lng: poliLon,
-  zoom: 8, //map is centered in the point 0, 0, zoom level 4
-  // style: "mapbox://styles/mapbox/navigation-preview-night-v2",
-  style: "mapbox://styles/beafiore/ck2rncqlt3lrv1cmkf6lw37g9",
+  lat: monteBiancoLat,
+  lng: monteBiancoLon,
+  zoom: 12,
+  style: "mapbox://styles/mapbox/satellite-v9",
+  // style: "mapbox://styles/beafiore/ck2rncqlt3lrv1cmkf6lw37g9",
 
 }
 
 function preload() {
   position = getCurrentPosition();
+  img = loadImage('assets/logoMonteBia.png');
 }
 
 function setup() {
@@ -30,16 +33,34 @@ function setup() {
 function draw() {
   clear()
 
+  push()
   var here = myMap.latLngToPixel(position.latitude, position.longitude);
-  fill(color("#ff6600"));
-  // strokeWeight(3);
-  textSize(14);
+  fill(color(col1));
+  noStroke()
+  textSize(20);
   ellipse(here.x, here.y, 30)
   text('Here you are', here.x - 40, here.y - 20);
 
-  var poli = myMap.latLngToPixel(poliLat, poliLon);
-  fill(color("#0066ff"));
-  ellipse(poli.x, poli.y, 30);
-  text('Politecnico di Milano', poli.x - 60, poli.y - 20);
+  var monteBianco = myMap.latLngToPixel(monteBiancoLat, monteBiancoLon);
+  img.resize(100, 0);
+  image(img, monteBianco.x - img.width / 2, monteBianco.y - 1.5*img.height);
+  fill(240);
+  textSize(30)
+  text('Monte Bianco', monteBianco.x - 60, monteBianco.y - 30);
+  text("4.808mt", monteBianco.x - 50, monteBianco.y - 5);
+  pop()
+
+  //distance between you and Monte Bianco
+  var howFar = calcGeoDistance(position.latitude, position.longitude, monteBiancoLat, monteBiancoLon, "km");
+  strokeWeight(6);
+  strokeCap(ROUND);
+  stroke(color("#2659a3"));
+  line(here.x, here.y, monteBianco.x, monteBianco.y)
+
+  //text
+  fill(240)
+  textSize(30)
+  textAlign(CENTER)
+  text("There are only " + Math.round(howFar) + "km between you and the top of Europe. Dream higher!", width/2, height-200)
 
 }
